61896d807d3716af84f78f261a26f4ca251e79c0
Zach Leatherman <zachleat@users.noreply.github.com>
2024-06-14 14:47:25 -0500

    Fix tests.

--- test/test.js
+++ test/test.js
@@ -1,59 +1,91 @@
 import test from 'node:test';
-import assert from "node:assert";
+import assert from "node:assert/strict";
 import fs from "node:fs";
 import { spawn } from "node:child_process";
 import { rimrafSync } from "rimraf";
 
-const SAMPLE_CONTENT = '"# Test content"';
+const SAMPLE_CONTENT = '# Test content';
 
-test('Baseline', (t) => {
-	let path = 'test/stubs-0/index.md';
-	let create = spawn("node", ['.', path, SAMPLE_CONTENT]);
-
-	create.on('close', () => {
-	  assert.ok(fs.existsSync(path));
-		assert.deepEqual(fs.readFileSync(path, "utf8"), SAMPLE_CONTENT);
-		rimrafSync("./test/stubs-0/");
+function waitForStreamClose(stream) {
+	return new Promise(resolve => {
+		stream.on("close", () => {
+			resolve();
+		});
 	});
+}
+
+test('Baseline double quotes', async (t) => {
+	t.after(() => {
+		rimrafSync("./test/stubs-dbl/");
+	});
+
+	let path = 'test/stubs-dbl/index.md';
+	let create = spawn("node", ['.', path, SAMPLE_CONTENT]);
+	await waitForStreamClose(create);
+
+	assert.ok(fs.existsSync(path));
+	assert.deepEqual(fs.readFileSync(path, "utf8"), SAMPLE_CONTENT);
 });
 
-test('Missing content', (t) => {
-	let path = 'test/stubs-err-c/index.md';
-	let create = spawn("node", ['.', path]);
+test('Baseline single quotes', async (t) => {
+	t.after(() => {
+		rimrafSync("./test/stubs-sgl/");
+	});
 
-	create.on('close', () => {
-	  assert.ok(!fs.existsSync(path));
+	let path = 'test/stubs-sgl/index.md';
+	let create = spawn("node", ['.', path, SAMPLE_CONTENT]);
+
+	await waitForStreamClose(create);
+
+	assert.ok(fs.existsSync(path));
+	assert.deepEqual(fs.readFileSync(path, "utf8"), SAMPLE_CONTENT);
+});
+
+test('Missing content', async (t) => {
+	t.after(() => {
 		rimrafSync("./test/stubs-err-c/");
 	});
+
+	let path = 'test/stubs-err-c/index.md';
+	let create = spawn("node", ['.', path]);
+	await waitForStreamClose(create);
+
+	assert.ok(!fs.existsSync(path));
 });
 
-test('Missing filename', (t) => {
-	let path = 'test/stubs-err-f/';
-	let create = spawn("node", ['.', path]);
-
-	create.on('close', () => {
-	  assert.ok(!fs.existsSync(path));
+test('Missing filename', async (t) => {
+	t.after(() => {
 		rimrafSync("./test/stubs-err-f/");
 	});
+
+	let path = 'test/stubs-err-f/';
+	let create = spawn("node", ['.', path]);
+	await waitForStreamClose(create);
+
+	assert.ok(!fs.existsSync(path));
 });
 
-test('Point to a directory', (t) => {
-	let path = 'test/stubs-err-d/';
-	let create = spawn("node", ['.', path, SAMPLE_CONTENT]);
-
-	create.on('close', () => {
-	  assert.ok(!fs.existsSync(path));
+test('Point to a directory', async (t) => {
+	t.after(() => {
 		rimrafSync("./test/stubs-err-d/");
 	});
+
+	let path = 'test/stubs-err-d/';
+	let create = spawn("node", ['.', path, SAMPLE_CONTENT]);
+	await waitForStreamClose(create);
+
+	assert.ok(!fs.existsSync(path));
 });
 
-test('Nested create', (t) => {
-	let path = 'test/stubs-1/nested/index.md';
-	let create = spawn("node", ['.', path, SAMPLE_CONTENT]);
-
-	create.on('close', () => {
-	  assert.ok(fs.existsSync(path));
-		assert.deepEqual(fs.readFileSync(path, "utf8"), SAMPLE_CONTENT);
-		rimrafSync("./test/stubs-1/");
+test('Nested create', async (t) => {
+	t.after(() => {
+		rimrafSync("./test/stubs-nested/");
 	});
+
+	let path = 'test/stubs-nested/nested/index.md';
+	let create = spawn("node", ['.', path, SAMPLE_CONTENT]);
+	await waitForStreamClose(create);
+
+	assert.ok(fs.existsSync(path));
+	assert.deepEqual(fs.readFileSync(path, "utf8"), SAMPLE_CONTENT);
 });
